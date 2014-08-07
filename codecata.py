891922834686fa2a07482549227f8581c417ce3d
CatharticPotatoz <tmelij@gmail.com>
2014-08-07 19:09:40 -0400

    dfghj

---
+++ codecata.py
@@ -0,0 +1,34 @@
+print("desfghj")
+d = "dfghj"
+print(d)
+print("frghjk" + d)
+f = 12
+print(f)
+print(str(f) + "fghjk")
+
+
+name = ""
+quest = ""
+def ask(q):
+    answer = input(q)
+    print("you said" + answer)
+    
+
+    
+def dead():
+    print("canst into the pits of whatever")
+generic_question()
+if name == "lancelot":
+    color = ask("whats your fav color")
+    if color == "blue":
+        print("pass")
+    else:
+        dead()
+elif name == "king aurthor":
+    speed = input("whats the speed of a swallow")
+    if speed == "what kind" or speed == "which kind":
+        print("theres more than one?")
+    else:
+        dead()
+else:
+    print("Fgfhjkl;")
